dcd1490eb4116dd196e243863d4e8508926e0408
Nikolay I <144357637+ipd0828@users.noreply.github.com>
2023-12-09 14:14:08 +0300

    Update app_book_end.py

--- app_book_end.py
+++ app_book_end.py
@@ -6,7 +6,6 @@ import pickle
 import streamlit as st 
 from PIL import Image 
 import datetime
-import catboost
 import lightgbm
 st.set_page_config(
         page_title="Расчет вероятности отмены бронирования", page_icon = 'favicon.ico'
